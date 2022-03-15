0bf4bacd76188b803612e594daccf9febe699ab3
Júlíus Reynald Björnsson <julius@laeknaromur.is>
2022-03-15 11:48:15 +0000

    fix gitignore

--- CaptiniAPI/settings.py
+++ CaptiniAPI/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = "django-insecure-+@x6r8b%j=r+&w&491(^xikv*t(vrzdfntr9uu)p!ritbr%-r=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
