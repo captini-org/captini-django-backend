41c259cd952f4e5de291d941bccd5858df359dc4
Júlíus Reynald Björnsson <julius@laeknaromur.is>
2022-03-15 11:56:00 +0000

    Remove hardcoded Django secret Key

--- CaptiniAPI/settings.py
+++ CaptiniAPI/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+@x6r8b%j=r+&w&491(^xikv*t(vrzdfntr9uu)p!ritbr%-r="
+# SECRET_KEY = "django-insecure-+@x6r8b%j=r+&w&491(^xikv*t(vrzdfntr9uu)p!ritbr%-r="
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -138,3 +136,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+import django_heroku
+django_heroku.settings(locals())
