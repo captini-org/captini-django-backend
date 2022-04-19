7e6c99b3edd36405bf5fc47175aac73cbd12ccc5
Júlíus Reynald Björnsson <julius@laeknaromur.is>
2022-04-19 13:59:05 +0200

    remove languages package from settings

--- CaptiniAPI/settings.py
+++ CaptiniAPI/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "languages",
     'django_better_admin_arrayfield',
     "rest_framework",
     "rest_framework.authtoken",
