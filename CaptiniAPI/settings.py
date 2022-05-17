62808078c525383f8317ccd197af751811198c5a
Júlíus Reynald Björnsson <julius@laeknaromur.is>
2022-05-17 11:06:36 +0000

    add rabbitMQ reciever script

--- CaptiniAPI/settings.py
+++ CaptiniAPI/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_extensions",
     "languages",
     'django_better_admin_arrayfield',
     "rest_framework",
