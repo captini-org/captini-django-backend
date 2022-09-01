bba41d2b9c0082a7869a9f809cb3dc6b148c0c20
Júlíus <julius@laeknaromur.is>
2022-09-01 13:06:36 +0000

    user details view

--- account/api/urls.py
+++ account/api/urls.py
@@ -5,13 +5,14 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from account.api.views import registration_view, logout_view, UserList
+from account.api.views import UserDetails, registration_view, logout_view, UserList
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     path('users/', UserList.as_view(), name='user-list'),
+    path('users/<int:pk>', UserDetails.as_view(), name='user-details'),
     # path('logout/', logout_view, name='logout'),
     path('register/', registration_view, name='register'),
 ]
