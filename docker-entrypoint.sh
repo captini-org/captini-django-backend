83342c4cdf784835264112550d4f7da704e5c23b
Róbert Kjaran <robert@kjaran.com>
2023-02-20 16:29:41 +0000

    Add Dockerfile for running devserver

---
+++ docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ $# -gt 0 ]; then
+  exec python manage.py "$@"
+else
+  python manage.py migrate
+  python manage.py createsuperuser
+  python manage.py makemigrations captini
+  python manage.py migrate
+
+  exec python manage.py runserver
+fi
