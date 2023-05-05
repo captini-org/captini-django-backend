2f8fa40e0b003c858de5e2c9ec1ac7cd332ada22
Júlíus <julius@laeknaromur.is>
2023-05-05 11:38:59 +0000

    add nested inline rendering for Tasks in Prompts

--- captini/admin.py
+++ captini/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Topic, Lesson, Prompt, Task, UserPromptScore, UserTaskRecording
+import nested_admin
 
 
 
@@ -11,19 +12,21 @@ class LessonsAdmin(admin.ModelAdmin):
     list_display = ('id', 'topic', 'subject', 'description', 'number')
     ordering = ['id', 'number']
 
-
-class PromptAdmin(admin.ModelAdmin):
-    list_display = ('id', 'lesson', 'prompt_number', 'number')
-    ordering = ['id', 'number']
-
-class TaskAdmin(admin.ModelAdmin):
+class TaskInline(nested_admin.NestedStackedInline):
+    model = Task
     list_display = ('id', 'prompt', 'task_text', 'audio_url', 'number')
     ordering = ['id', 'prompt', 'number']
+    extra = 0
     
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'prompt':
             kwargs['queryset'] = Prompt.objects.order_by('prompt_number')
-        return super(TaskAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+        return super(TaskInline, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
+class PromptAdmin(nested_admin.NestedModelAdmin):
+    list_display = ('id', 'lesson', 'prompt_number', 'number')
+    ordering = ['id', 'number']
+    inlines = [TaskInline]
 
 
 class UserTaskRecordingAdmin(admin.ModelAdmin):
@@ -39,6 +42,5 @@ class UserPromptScoreAdmin(admin.ModelAdmin):
 admin.site.register(Topic, TopicAdmin)
 admin.site.register(Lesson, LessonsAdmin)
 admin.site.register(Prompt, PromptAdmin)
-admin.site.register(Task, TaskAdmin)
 admin.site.register(UserTaskRecording, UserTaskRecordingAdmin)
 admin.site.register(UserPromptScore, UserPromptScoreAdmin)
