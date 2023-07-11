2e06d4c62eff4955b8bf8f932bb17a2e4700c915
mitaerika <72701220+mitaerika@users.noreply.github.com>
2023-07-11 16:58:47 +0000

    validate registration data

--- account/api/serializers.py
+++ account/api/serializers.py
@@ -17,14 +17,17 @@ class RegistrationSerializer(serializers.ModelSerializer):
         
         password = self.validated_data['password']
         password2 = self.validated_data['password2']
-        
         if password != password2:
             raise serializers.ValidationError({'error': 'Passwords do not match!'})
         
         if User.objects.filter(email=self.validated_data['email']).exists():
             raise serializers.ValidationError({'error': 'That email already in use!'})
         
-        account = User(email=self.validated_data['email'], username=self.validated_data['username'])
+        account = User(email=self.validated_data['email'], username=self.validated_data['username'],
+        first_name=self.validated_data['first_name'],
+        last_name=self.validated_data['last_name'],
+        birthday=self.validated_data['birthday'],
+        nationality=self.validated_data['nationality'])
         account.set_password(password)
         account.save()
         return account
